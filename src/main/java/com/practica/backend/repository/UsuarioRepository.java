5dceebc2fcefd10a9a242eff1f6dc9a484dc9ea0
PipeFelipe1234 <nominapipe@gmail.com>
2026-01-21 14:01:39 -0500

    Preparar Cors y Conexion para Railway

--- src/main/java/com/practica/backend/repository/UsuarioRepository.java
+++ src/main/java/com/practica/backend/repository/UsuarioRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByIdentificacion(String identificacion);
+    // hola
 }
