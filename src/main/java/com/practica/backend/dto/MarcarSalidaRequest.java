4f4878c7162e8f01d5409a872b25527bc16ade8b
PipeFelipe1234 <nominapipe@gmail.com>
2026-01-25 17:43:34 -0500

    quitar el mapeo del checkout

--- src/main/java/com/practica/backend/dto/MarcarSalidaRequest.java
+++ src/main/java/com/practica/backend/dto/MarcarSalidaRequest.java
@@ -1,11 +1,9 @@
 package com.practica.backend.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record MarcarSalidaRequest(
-                @JsonProperty("latitudCheckout") Double latitud,
-                @JsonProperty("longitudCheckout") Double longitud,
-                @JsonProperty("precisionMetrosCheckout") Double precisionMetros,
+                Double latitud,
+                Double longitud,
+                Double precisionMetros,
                 String reporte,
                 String picture) {
 }
